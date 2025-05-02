d2c2b9a9e3ac93a3c0b11a95116bc2866ce248bd
Sahitya Neupane <neupanesahitya1@gmail.com>
2025-05-02 11:56:49 -0400

    created basic adapter for customStoragePlugin

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -11,6 +11,7 @@ import { Users } from './collections/Users'
 import { Media } from './collections/Media'
 import { cloudStoragePlugin } from '@payloadcms/plugin-cloud-storage'
 import { v2 as cloudinary } from 'cloudinary'
+import type { HandleUpload } from '@payloadcms/plugin-cloud-storage/types'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -20,6 +21,32 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+// creating cloudinary adapter
+const cloudinaryAdapter = () => ({
+  name: 'cloudinary-adapter',
+  async handleUpload({
+    file,
+    collection,
+    data,
+    req,
+    clientUploadContext,
+  }: Parameters<HandleUpload>[0]) {
+    console.log('File:', file)
+    console.log('Collection:', collection)
+    console.log('Data:', data)
+
+    try {
+      // your upload logic here
+    } catch (err) {
+      console.error('Upload Error', err)
+    }
+  },
+  async handleDelete() {},
+  staticHandler() {
+    return new Response('Not implemented', { status: 501 })
+  },
 })
 
 export default buildConfig({
@@ -41,6 +68,14 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
+    cloudStoragePlugin({
+      collections: {
+        media: {
+          adapter: cloudinaryAdapter,
+          disableLocalStorage: true,
+        },
+      },
+    }),
 
     // storage-adapter-placeholder
   ],
